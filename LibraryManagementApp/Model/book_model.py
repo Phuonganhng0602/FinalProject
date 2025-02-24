0dc7e99e05de4e0be9024c5771ea75cae079e0dc
Phuonganhng0602 <anhnp23406@st.uel.edu.vn>
2025-02-24 23:02:37 +0700

    updated model book

---
+++ LibraryManagementApp/Model/book_model.py
@@ -0,0 +1,19 @@
+#import database
+
+class Book:
+    def __init__(self, title=None, author=None, category=None, published_year=None, quantity=None):
+        self.title = title
+        self.author = author
+        self.category = category
+        self.pulished_year = published_year
+        self.quantity = quantity
+        # self.db = Database()
+    def save(self):
+        pass
+
+    def get_book_by_id(book_id):
+        pass
+
+    def get_book_by_title(title):
+        pass
+
