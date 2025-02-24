6c53a5f3cb95cca3c60baa0d950e522f4a8325f2
Buggie404 <judyhoang1807@gmail.com>
2025-02-24 11:08:37 +0700

    updated rule/structire for each part

---
+++ LibraryManagementApp/Model/example_model.py
@@ -0,0 +1,15 @@
+"""Struture của file model"""
+
+#import database: from example_db import example_class
+#đối với admin import thêm model user: from user_model import User
+
+#class Example:
+#    def __init__(self, các data trong database): Lưu data từ database
+#        self.các data trong database = các data trong database
+#        self.db = example_class()
+#   def save(self): Lưu tạm thời vào model
+#       
+#   def chức_năng(self, các data cần)
+
+# Hàm __init__ của model Admin -> tham khảo link Model trong doc
+# Model Admin, không cần def save, các hàm chức năng code như bình thường
---
+++ LibraryManagementApp/View/exmaple_view.py
@@ -0,0 +1,2 @@
+# Đặt tên View theo các chức năng chính. Ví dụ: BM_main.py -> màn hình chính của Book Management
+# Trong View sẽ có file .ui và .py (không gồm Extend.py). Các .py generate từ .ui sẽ có tên giống nhai
